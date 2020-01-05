aea847dae1a69d9bd892eea9443985ad0c8be64f
Nils Johnson <nilsjohnson328@gmail.com>
2020-01-05 15:09:02 -0500

    javascript security updates, started config for https

--- server/server.js
+++ server/server.js
@@ -1,6 +1,39 @@
+/*
+If running in production on server, pass 'prudction' as an argument:
+	$ node server.js production
+This will allow listening for https requests, otherwise it will just do http.	
+*/
+const PRODUCTION = "A unique String! :)";
+let mode;
+
+if (process.argv.length > 2 && process.argv[2] === "production") {
+	console.log("Server running in prodcution. Will listen for https requests.");
+	mode = PRODUCTION;
+}
+else {
+	console.log("Server running in DEV mode. Will not listen for https requests");
+}
+
 const express = require('express');
 const path = require('path');
 const app = express();
+
+const HTTP_PORT_NUM = 3000;
+const HTTPS_PORT_NUM = 443;
+
+// for https
+let FULL_CHAIN;
+let PRIVATE_KEY;
+let OPTIONS = {};
+
+if (mode == PRODUCTION) {
+	FULL_CHAIN = '/etc/letsencrypt/live/www.gillccwoodworks.com/fullchain.pem';
+	PRIVATE_KEY = '/etc/letsencrypt/live/www.gillccwoodworks.com/privkey.pem';
+	OPTIONS = {
+		cert: fs.readFileSync(FULL_CHAIN),
+		key: fs.readFileSync(PRIVATE_KEY)
+	};
+}
 
 app.use(express.static(path.join(__dirname, '../build'), { index : false })); // index : false is to allow request for the webroot to get caught by 'app.get('/*', function(req, res)', allowing http to https redirects
 app.use(express.static(path.join(__dirname, '../public'), { index : false })); 
@@ -8,10 +41,31 @@ app.use(express.static(path.join(__dirname, '../public'), { index : false }));
 
 
 app.get('/*', function(req, res) {
+    if(mode === PRODUCTION) {
+      if(!req.secure){
+				res.redirect("https://" + req.headers.host + req.url);
+			}
+			else if(!req.headers.host.startsWith("www")){
+				res.redirect("https://www." + req.headers.host + req.url);
+			}
+			else {
+				res.sendFile(path.join(__dirname, '../public', 'index.html'));
+			}
+    }
   	res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+/*
+Starts server
+*/
+app.listen(HTTP_PORT_NUM, function() {
+  console.log('Listening on port 3000 for requests');
 });
+
+/*
+Starts listening for https requests
+*/
+if (mode == PRODUCTION) {
+	https.createServer(OPTIONS, app).listen(HTTPS_PORT_NUM);
+}
