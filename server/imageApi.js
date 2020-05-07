fb432c64d238b419a260e5e3746dcf06b7a3af6a
Nils <nilsjohnson328@gmail.com>
2020-05-07 16:55:38 -0400

    cleaned up carousel image api

--- server/imageApi.js
+++ server/imageApi.js
@@ -1,64 +1,147 @@
+
 const fs = require('fs');
-const { app } = require("./server");
 const bodyParser = require('body-parser');
-const fileUtil = require('./fileUtil.js');
 const busboy = require('connect-busboy');
 const path = require('path');
 const shell = require('shelljs');
+const { spawn } = require('child_process');
+
+const { app } = require("./server");
+const fileUtil = require('./fileUtil.js');
 
 app.use(bodyParser.json());
 app.use(busboy())
 
-const CAROUSEL_IMG_FILE_NAME = 'carousel.json';
-let carouselImages = null;
 
+const CAROUSEL_IMG_FILE_NAME = 'carousel.json';
+const CAROUSEL_IMAGE_DIR = path.join(__dirname, '../public/');
+const PUBLIC_PATH = "carouselImg/";
+let carouselImages = null;
 const CAROUSEL_IMG_WIDTH = 900;
 const CAROUSEL_IMG_HIGHT = 600;
+const LEFT = "LEFT", RIGHT = "RIGHT";
 
 /**
- * Loads the carousel images from the file
- * must be called on start
+ * Reads the carousel images from disk. After calling, carouselImages will
+ * not be null. 
  */
-function loadCarouselImages() {
+function loadImages() {
 	carouselImages = fileUtil.readObj(CAROUSEL_IMG_FILE_NAME);
-	
 	if(carouselImages == null) {
 		carouselImages = [];
 	}
 }
 
+/**
+ * Returns the carousel image array, or an empty array if there are none.
+ */
+function getCarouselImages() {
+	if(carouselImages == null) {
+		loadImages();
+	}
+
+	return carouselImages;
+}
+
+/**
+ * @param {string} image Image to add to carousel, i.e "fileName.jpg"
+ */
 function addCarouselImage(image) {
 	carouselImages.push(image);
 	fileUtil.writeObj(carouselImages, CAROUSEL_IMG_FILE_NAME);
 }
 
-function getCarouselImages() {
-	return carouselImages;
+/**
+ * @param {string} image File name of image to delete. I.e 'filename.jpg' 
+ */
+function deleteImage(image) {
+	for(let i = 0; i < carouselImages.length; i++) {
+		if(getCarouselImages()[i] === image) {
+			getCarouselImages().splice(i, 1);
+			let imgPath = CAROUSEL_IMAGE_DIR + image;
+			fileUtil.writeObj(getCarouselImages(), CAROUSEL_IMG_FILE_NAME);
+			fileUtil.deleteFile(imgPath);
+			return;
+		}
+	}
+
+	throw("No image to remove.");
 }
 
-const { spawn } = require('child_process');
+/**
+ * 
+ * @param {number} direction Left or right. 
+ * @param {string} image Filename of image. 
+ */
+function moveCarouselImage(direction, image) {
+	let imageIndex = 0;
+	let found = false;
+
+	// we directly access carousel images, so we make sure its loaded.
+	loadImages();
+
+	while (imageIndex < carouselImages.length) {
+		if (carouselImages[imageIndex] === image) {
+			found = true;
+			break;
+		}
+		else {
+			imageIndex++;
+		}
+	}
+
+	if (found) {
+		let rightIndex, leftIndex, temp;
+
+		if (imageIndex === 0) {
+			leftIndex = carouselImages.length - 1;
+			rightIndex = imageIndex + 1;
+		}
+		else if (imageIndex === carouselImages.length - 1) {
+			rightIndex = 0;
+			leftIndex = imageIndex - 1;
+		}
+		else {
+			rightIndex = imageIndex + 1;
+			leftIndex = imageIndex - 1;
+		}
+
+		temp = carouselImages[imageIndex];
+		if (direction === RIGHT) {
+			carouselImages[imageIndex] = carouselImages[rightIndex];
+			carouselImages[rightIndex] = temp;
+		}
+		if (direction === LEFT) {
+			carouselImages[imageIndex] = carouselImages[leftIndex];
+			carouselImages[leftIndex] = temp;
+		}
+		// save this ordering
+		fileUtil.writeObj(carouselImages, CAROUSEL_IMG_FILE_NAME);
+	}
+	else {
+		console.log("Can't move image. Image not found");
+	}
+}
 
 /*
-API To handle carousel uploads
+API endpoint To handle carousel uploads
 */
 app.post('/api/carouselUpload', function (req, res, next) {
 	console.log("image upload api hit");
 	let fstream;
 	req.pipe(req.busboy);
 	req.busboy.on('file', function (fieldname, file, filename) {
-		console.log("Uploading: " + filename);
+		console.log(`Uploading File: ${filename}`);
 		let src = '/tmp/' + filename;
-		let dst = path.join(__dirname, '../public/carouselImg/' + filename);
-		let finalAddr = '/carouselImg/' + filename;
+		let dst = CAROUSEL_IMAGE_DIR + PUBLIC_PATH + filename;
+		let saveName = PUBLIC_PATH + filename;
 
-		// does not allow duplicate titles
+		// rename images as to not overwrite files with same name.
 		let i = 2;
-		while (carouselImages.includes(finalAddr)) {
-			console.log("exists");
-			filename = "(" + i + ")" + filename;
-			src = '/tmp/' + filename;
-			dst = path.join(__dirname, '../public/carouselImg/' + filename);
-			finalAddr = '/carouselImg/' + filename;
+		while (getCarouselImages().includes(saveName)) {
+			saveName = PUBLIC_PATH + "(" + i + ")" + filename;
+			console.log(`Duplicate filename found. Renaming it "${saveName}"`);
+			dst = CAROUSEL_IMAGE_DIR + saveName;
 			i++;
 		}
 		//Path where image will be uploaded
@@ -67,24 +150,28 @@ app.post('/api/carouselUpload', function (req, res, next) {
 		fstream.on('close', function () {
 			console.log("'" + filename + "' uploaded..resize next");
 			resize(src, dst, CAROUSEL_IMG_WIDTH, CAROUSEL_IMG_HIGHT, function (destination) {
-				res.json({ address: finalAddr });
-				addCarouselImage(finalAddr);
-				shell.rm(src); // delete temporary file
+				res.json({ address: saveName });
+				addCarouselImage(saveName);
+				shell.rm(src); // delete temporary file TODO use fileUtil
 			});
 		});
 	});
 });
 
+/**
+ * API endpoint to get the carousel images
+ */
 app.get('/api/carouselImages', function(req, res) {
-    console.log("get image api hit: ");
-	res.json(carouselImages);
+	console.log("get image api hit: ");
+	res.json(getCarouselImages());
 
 });
 
 /*
-API to shift carousel images left or right
+API endpoint to shift carousel images left or right
 */
 app.put('/api/carouselImgs/move', function (req, res) {
+	console.log(req.body);
 	let result;
 	try {
 		moveCarouselImage(req.body.direction, req.body.image);
@@ -118,69 +205,9 @@ app.delete('/api/carouselImgDelete', function (req, res) {
 	res.json({ result: result });
 });
 
-function deleteImage(image) {
-	for(let i = 0; i < carouselImages.length; i++) {
-		if(carouselImages[i] === image) {
-			carouselImages.splice(i, 1);
-			let imgPath = path.join(__dirname, '../public/' + image);
-			fileUtil.writeObj(carouselImages, CAROUSEL_IMG_FILE_NAME);
-			fileUtil.deleteFile(imgPath);
-			return;
-		}
-	}
 
-	throw("No image to remove.");
-}
 
-/*
-moves an image
-*/
-function moveCarouselImage(direction, image) {
-	let imageIndex = 0;
-	let found = false;
 
-	while (imageIndex < carouselImages.length) {
-		if (carouselImages[imageIndex] === image) {
-			found = true;
-			break;
-		}
-		else {
-			imageIndex++;
-		}
-	}
-
-	if (found) {
-		let rightIndex, leftIndex, temp;
-
-		if (imageIndex === 0) {
-			leftIndex = carouselImages.length - 1;
-			rightIndex = imageIndex + 1;
-		}
-		else if (imageIndex === carouselImages.length - 1) {
-			rightIndex = 0;
-			leftIndex = imageIndex - 1;
-		}
-		else {
-			rightIndex = imageIndex + 1;
-			leftIndex = imageIndex - 1;
-		}
-
-		temp = carouselImages[imageIndex];
-		if (direction === "RIGHT") {
-			carouselImages[imageIndex] = carouselImages[rightIndex];
-			carouselImages[rightIndex] = temp;
-		}
-		if (direction === "LEFT") {
-			carouselImages[imageIndex] = carouselImages[leftIndex];
-			carouselImages[leftIndex] = temp;
-		}
-		// save this ordering
-		fileUtil.writeObj(carouselImages, CAROUSEL_IMG_FILE_NAME);
-	}
-	else {
-		console.log("Can't move image. Image not found");
-	}
-}
 
 /*
 To resize images. Spawns a Java program. 
@@ -218,5 +245,3 @@ function resize(src, dst, width, height, callback) {
 	});
 }
 
-
-loadCarouselImages();
